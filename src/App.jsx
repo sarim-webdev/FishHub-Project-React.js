313a1516d033ed61b4aba9418eedf01014d1a7d3
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-03-05 12:19:53 +0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './Components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { CartProvider } from "./Context/CartContext"
+
+import MainLayout from "./Layouts/MainLayout"
+import AuthLayout from "./Layouts/AuthLayout"
+
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Products from "./Pages/Products"
+import Cart from "./Pages/Cart"
+import Checkout from "./Pages/Checkout"
+import FAQS from "./Pages/FAQS"
+import Reviews from "./Pages/Reviews"
+import Contact from "./Pages/Contact"
+
+import Login from "./Pages/Auth/Login"
+import Signup from "./Pages/Auth/Signup"
 import "./App.css"
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Products from './Pages/Products'
-import Cart from './Pages/Cart'
-import { CartProvider } from './Context/CartContext'   
-import Checkout from './Pages/Checkout'
-import FAQS from './Pages/FAQS'
-import Reviews from './Pages/Reviews'
-import Contact from './Pages/Contact'
-import Login from './Pages/Auth/Login'
-import Signup from './Pages/Auth/Signup'
-import Footer from './Components/Footer'
 
 const App = () => {
   return (
-    <CartProvider>   
+    <CartProvider>
       <BrowserRouter>
-        <Navbar/>
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/products' element={<Products/>} />
-          <Route path='/faqs' element={<FAQS/>} />
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/checkout' element={<Checkout/>} />
-          <Route path='/reviews' element={<Reviews/>} />
-          <Route path='/contact' element={<Contact/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signup' element={<Signup/>} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/faqs" element={<FAQS />} />
+            <Route path="/reviews" element={<Reviews />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+
         </Routes>
-        <Footer/>
       </BrowserRouter>
     </CartProvider>
   )
---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
+import { Outlet } from "react-router-dom"
+
+const MainLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default MainLayout
