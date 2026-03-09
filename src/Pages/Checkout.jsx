ba6368596ad1ad9d7c776f106be40eba8ce01412
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-03-09 14:21:19 +0500

    react

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useCart } from "../Context/CartContext";
 import { FaCreditCard } from "react-icons/fa";
-import JazzCashLogo from "../assets/jazzcash-image.jpeg"; 
-import EasypaisaLogo from "../assets/easypaise-image.jpeg"; 
+import JazzCashLogo from "../assets/jazzcash-image.png"; 
+import EasypaisaLogo from "../assets/easypaise-image.png"; 
 
 const Checkout = () => {
   const { cart, total, clearCart } = useCart();
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import heroImage from "../assets/hero-section-image.jpeg"
+import heroImage from "../assets/hero-section-image.png"
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
